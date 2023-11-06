033572d1440a2cfd579468235d69b638c4b8737c
Olga <c_h_e_r_r_y@tut.by>
2023-11-06 21:10:07 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let isCheck;
+  // console.log(bracketsConfig.length);
+   let Stack = [];
+   if (!str) {
+     return;
+   }
+ 
+   else {
+   Stack.push(str[0]);
+   
+   for (let i = 1; i < str.length; i++) {
+  //   console.log(str[i]);
+     Stack.push(str[i]);
+     bracketsConfig.forEach(element => {
+       if (str[i] == element[1] && Stack[Stack.length-2] == element[0]) {
+         Stack.pop();
+         Stack.pop();
+       }
+     });
+ 
+   }
+ } 
+ 
+ Stack.length > 0 ? isCheck = false : isCheck = true;
+ //console.log(isCheck);
+ return isCheck;
 }
